445ae25fafd6b1917076de70651ff02b75a79393
Nick Alcock <nick.alcock@oracle.com>
2011-05-23 17:36:45 +0100

    Turn runtest.sh into a crude regression tester.
    If you have multiple build-* directories in place, runtest will now run all
    tests against each of them, and print any differences in output or in
    intermediate representation (picking a random one to compare against).
    (This is unashamedly GNU diff-dependent.)
    
    Differences in pointer addresses or anything else looking hexadecimal
    are filtered out.
    
    Also made slightly more bash-idiomatic.
    
    Finally, add a .gitignore which filters out some (but not yet all) of the build
    debris.

--- runtest.sh
+++ runtest.sh
@@ -1,39 +1,123 @@
 #!/bin/bash
+#
+# runtest.sh -- A dtrace tester and regression tester.
+#
+#               Runs a bunch of .d scripts and tests their results.
+#               If more than dtrace build directory is available,
+#               runs them each in turn, comparing their output
+#               and generated intermediate representation.
+#
+
+# Sanitize the shell.
+
+shopt -s nullglob
+unset CDPATH
+
+#
+# get_dir_name
+#
+# Pick a unique temporary directory name to stick things in.
+# Returns results via `echo'; must be called via substitution.
+#
+# Respect TMPDIR if set, as POSIX requires.
+
+get_dir_name()
+{
+    typeset tmpname;                  # A subdir name being tested for uniqueness
+
+    false;                            # Starting state - reset $?
+    while [[ $? -ne 0 ]]; do
+        tmpname=${TMPDIR:-/tmp}/runtest.${RANDOM}
+        mkdir $tmpname                # Test-and-set it
+    done
+
+    echo $tmpname                     # Name is unique, return it
+}
+
+# Make a temporary directory for intermediate result storage.
+
+typeset tmpdir=$(get_dir_name)
+trap 'cd; rm -rf ${tmpdir}; exit' INT QUIT TERM SEGV EXIT
+
 pid=`echo $$`
-dtrace=./build-*/dtrace
-if [ ! -f $dtrace ]; then
-	echo "No dtrace command line";
+dtrace="./build-*/dtrace"
+if [[ -z $dtrace ]]; then
+	echo "No dtraces available.";
 	exit 1;
 fi
 
-for i in $(ls ./demo/*/*.d)
-do
-cmd=$dtrace\ -e\ -s\ $i
-	if [ $i == "./demo/buf/ring.d" ] ||
-	   [ $i == "./demo/sched/whererun.d" ]; then
-		cmd=$cmd\ /bin/date
-	elif [ $i == "./demo/user/badopen.d" ] ||
-	     [ $i == "./demo/struct/rwinfo.d" ] ||
-	     [ $i == "./demo/script/tracewrite.d" ] ||
-	     [ $i == "./demo/profile/profpri.d" ] ||
-	     [ $i == "./demo/intro/rwtime.d" ] ||
-	     [ $i == "./demo/intro/trussrw.d" ]; then
-		cmd=$cmd\ $pid
-	elif [ $i == "./demo/user/errorpath.d" ]; then
-		cmd=$cmd\ $pid\ _chdir
-	elif [ $i == "./demo/sched/pritime.d" ]; then
-		cmd=$cmd\ $pid\ 1
-	elif [ $i == "./demo/user/userfunc.d" ]; then
-		cmd=$cmd\ $pid\ execute
-	elif [ $i == "./demo/struct/kstat.d" ]; then
-		cmd=$dtrace\ -q\ -e\ -s\ $i\ `pgrep mpstat`
-	elif [ $i = "./demo/user/libc.d" ]; then
-		cmd=$cmd\ -c\ /bin/date
-	fi
-$cmd
-echo $cmd
+# More than one dtrace tree -> verify identical results for each dtrace.
+
+regression=
+first_dtest_dir=
+if [[ $(echo $dtrace | wc -w) -gt 1 ]]; then
+    regression=t
+fi
+
+# Loop over each dtrace test. Non-regression-tests print all output:
+# regression tests only print output when something is different.
+
+for tst in $(echo demo/*/*.d); do
+    for dt in $dtrace; do
+        dt_flags="-e -s $tst"
+
+        case "$tst" in
+            ./demo/buf/ring.d) ;&
+            ./demo/sched/whererun.d) dt_flags="$dt_flags /bin/date";;
+
+            ./demo/user/badopen.d) ;&
+            ./demo/struct/rwinfo.d) ;&
+            ./demo/script/tracewrite.d) ;&
+            ./demo/profile/profpri.d) ;&
+            ./demo/intro/rwtime.d) ;&
+            ./demo/intro/trussrw.d) dt_flags="$dt_flags $pid";;
+
+            ./demo/user/errorpath.d) dt_flags="$dt_flags $pid _chdir";;
+
+            ./demo/sched/pritime.d) dt_flags="$dt_flags $pid 1";;
+
+            ./demo/user/userfunc.d) dt_flags="$dt_flags $pid execute";;
+
+            ./demo/struct/kstat.d) dt_flags="-q -e -s $tst $(pgrep mpstat)";;
+
+            ./demo/user/libc.d) dt_flags="$dt_flags -c /usr/bin/date";;
+        esac
+
+        if [[ -z $regression ]]; then
+            # Non-regression-tests just print the output.
+
+            echo $dt $dt_flags
+            $dt $dt_flags
+        else
+            # Regression tests run with intermediate results displayed, and
+            # output redirected to a per-test, per-dtrace temporary file.
+
+            dtest_dir="$tmpdir/$(basename $(dirname $dt))"
+            logfile="$(echo $tst | sed 's,/,-,g').log"
+            mkdir -p $dtest_dir
+            echo $dt_flags > $dtest_dir/$logfile
+            echo >> $dtest_dir/$logfile
+            $dt -S $dt_flags 2>&1 | sed 's,0x[0-9a-f][0-9a-f]*,{ptr},g' >> $dtest_dir/$logfile
+
+            if [[ -z $first_dtest_dir ]]; then
+                first_dtest_dir=$dtest_dir
+            fi
+
+        fi
+    done
 done
-#Test grap $pid
-cmd=$dtrace\ -e\ -p\ $pid
-$cmd
-echo $cmd
+
+if [[ -z $regression ]]; then
+    #Test grep $pid
+    echo $dtrace -e -p $pid
+    $dtrace -e -p $pid
+else
+    # Regtest comparison. Skip one directory and diff all the others
+    # against it.
+
+    for name in $tmpdir/*; do
+        [[ $name = $first_dtest_dir ]] && continue
+
+        diff -urN $first_dtest_dir $name
+    done
+fi
