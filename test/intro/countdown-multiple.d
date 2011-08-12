1925ab11dc1da322a1575ee5084651394bfcc99b
Nick Alcock <nick.alcock@oracle.com>
2011-08-12 18:42:51 +0100

    Test associative arrays.
    Multiple parallel tick firings too.

--- test/intro/countdown-multiple.d
+++ test/intro/countdown-multiple.d
@@ -26,15 +26,40 @@
 
 dtrace:::BEGIN
 {
-	i = 20;
+	i = 40;
+        t3 = 0;
+        t5 = 0;
+        t7 = 0;
 }
 
-profile:::tick-1sec
+profile:::tick-3hz
 /i > 0/
 {
-        i-=2;
-        @quant["quantized countdown"]=quantize(i);
-        @lquant["linear quantized countdown"]=lquantize(i,0,20,1);
+        i--;
+        t3 += 3;
+        @quant["quantized firings"]=quantize(i);
+        hits[t3%5]++;
+        @lquant["linear quantized firings"]=lquantize(i,0,40,1);
+}
+
+profile:::tick-5hz
+/i > 0/
+{
+        i--;
+        t5 += 5;
+        @quant["quantized firings"]=quantize(i);
+        hits[t5%5]++;
+        @lquant["linear quantized firings"]=lquantize(i,0,40,1);
+}
+
+profile:::tick-7hz
+/i > 0/
+{
+        i--;
+        t7 += 7;
+        @quant["quantized firings"]=quantize(i);
+        hits[t7%5]++;
+        @lquant["linear quantized firings"]=lquantize(i,0,40,1);
 }
 
 profile:::tick-1sec
@@ -42,4 +67,10 @@ profile:::tick-1sec
 {
 	trace("blastoff!");
 	exit(0);
+}
+
+dtrace:::END
+{
+    printf ("\n\nHits mod 5: (%i, %i, %i, %i, %i)\n",
+             hits[0], hits[1], hits[2], hits[3], hits[4]);
 }
