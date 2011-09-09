c29ccb52122a8ca96b61d402871d3a435e2349dd
Nick Alcock <nick.alcock@oracle.com>
2011-09-09 17:40:03 +0100

    Test dtrace error handling in the face of null pointer exceptions.
    This test already existed: I just hooked up the .r and (fake) .t file to turn it
    into a proper regression test.

---
+++ test/dtrace/error.r
@@ -0,0 +1,7 @@
+                   FUNCTION:NAME
+                          :ERROR Hit an error!
+
+
+-- @@stderr --
+dtrace: script 'test/dtrace/error.d' matched 2 probes
+dtrace: error on enabled probe ID 1 (ID 1: dtrace:::BEGIN): invalid kernel access in action #1 at DIF offset 16
