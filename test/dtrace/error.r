370512634f6a2f5bc1ecfd81c1d8f38a46444a9e
Shujing Zhao <pearly.zhao@oracle.com>
2011-12-30 10:33:32 -0500

    Fix the dtrace error messages.

--- test/dtrace/error.r
+++ test/dtrace/error.r
@@ -4,4 +4,4 @@
 
 -- @@stderr --
 dtrace: script 'test/dtrace/error.d' matched 2 probes
-dtrace: error on enabled probe ID 1 (ID 1: dtrace:::BEGIN): invalid kernel access in action #1 at DIF offset 16
+dtrace: error on enabled probe ID 1 (ID 1: dtrace:::BEGIN): invalid address ({ptr}) in action #1 at DIF offset 16
