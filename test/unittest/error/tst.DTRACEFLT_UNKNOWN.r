b00853112297f249cc0250825c236ac2471b16ae
Kris Van Hees <kris.van.hees@oracle.com>
2012-01-12 16:04:07 -0500

    Fixed the expected output from the test - indicate a bad address error.

--- test/unittest/error/tst.DTRACEFLT_UNKNOWN.r
+++ test/unittest/error/tst.DTRACEFLT_UNKNOWN.r
@@ -1,5 +1,5 @@
-The arguments are 2 2 4 6 64
+The arguments are 2 2 4 1 64
 The value of arg4 = 0
 
 -- @@stderr --
-dtrace: error on enabled probe ID 2 (ID 1: dtrace:::BEGIN): invalid kernel access in action #2 at DIF offset 4
+dtrace: error on enabled probe ID 2 (ID 1: dtrace:::BEGIN): invalid address ({ptr}) in action #2 at DIF offset 4
