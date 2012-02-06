76863d1f261f43ee985207aff930099ee5fb3a72
Nick Alcock <nick.alcock@oracle.com>
2012-02-06 16:54:21 +0000

    Handle test/unittest/buffering/tst.resize*.d properly.
    These tests attempt to allocate a gigantic result, aggregation, or speculation
    buffer, and verify that DTrace shrinks it to something sane and keeps running.
    
    Unfortunately these tests pushed my test system into swap, and were then killed
    by the test timeout before they produced any results.  I didn't look closely
    enough at the tests, and presumed that a quiet exit without results was a useful
    result (what else could you expect of an attempt to allocate 100 terabytes?).
    But I was wrong: these tests should succeed, and generate results.  If I boost
    the test timeout far enough for these tests (I picked 500s), I see this too.
    
    By an oversight, the once-crashing-the-kernel tst.resize3.d had no .r file at
    all, so no check was made on its results and it did not (spuriously) fail.
    Results have now been captured from this test too.

---
+++ test/unittest/buffering/tst.resize2.r
@@ -0,0 +1,7 @@
+                   FUNCTION:NAME
+                          :BEGIN 
+
+  dtrace                                                            1
+-- @@stderr --
+dtrace: script 'test/unittest/buffering/tst.resize2.d' matched 1 probe
+dtrace: aggregation size lowered to 200m
