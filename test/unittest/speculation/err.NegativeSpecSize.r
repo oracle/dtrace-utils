e313f0c000692e1dc480560b81cd2d8cf7e4404c
Kris Van Hees <kris.van.hees@oracle.com>
2012-09-19 18:22:54 -0400

    Remove some tests that duplicate test cases.
    Test err.NegativeSpecSize.d duplicates test err.D_PRAGMA_OPTSET.NegSpecSize.d
    Test tst.bug-1001148.SpecSizeVariations.d duplicates test tst.SpecSizeVariations2.d

--- test/unittest/speculation/err.NegativeSpecSize.r
+++
@@ -1,2 +0,0 @@
--- @@stderr --
-dtrace: failed to compile script test/unittest/speculation/err.NegativeSpecSize.d: line 40: failed to set option 'specsize' to '-10': Invalid value for specified option
