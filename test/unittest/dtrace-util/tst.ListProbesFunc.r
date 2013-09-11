bc419625b806d26be5d58113220fe497068a4d6c
Kris Van Hees <kris.van.hees@oracle.com>
2013-09-11 14:30:03 -0400

    Remove expected results for tst.stmts; fix expected results for probe lists.
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/dtrace-util/tst.ListProbesFunc.r
+++ test/unittest/dtrace-util/tst.ListProbesFunc.r
@@ -1,43 +1,43 @@
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
- XX    syscall                                               write entry
- XX    syscall                                               write return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
+ XX    syscall           vmlinux                             write entry
+ XX    syscall           vmlinux                             write return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
- XX    syscall                                               readv entry
- XX    syscall                                               readv return
- XX    syscall                                            readlink entry
- XX    syscall                                            readlink return
- XX    syscall                                           readahead entry
- XX    syscall                                           readahead return
- XX    syscall                                          readlinkat entry
- XX    syscall                                          readlinkat return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
+ XX    syscall           vmlinux                             readv entry
+ XX    syscall           vmlinux                             readv return
+ XX    syscall           vmlinux                          readlink entry
+ XX    syscall           vmlinux                          readlink return
+ XX    syscall           vmlinux                         readahead entry
+ XX    syscall           vmlinux                         readahead return
+ XX    syscall           vmlinux                        readlinkat entry
+ XX    syscall           vmlinux                        readlinkat return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                               write entry
- XX    syscall                                               write return
+ XX    syscall           vmlinux                             write entry
+ XX    syscall           vmlinux                             write return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
    ID   PROVIDER            MODULE                          FUNCTION NAME
- XX    syscall                                                read entry
- XX    syscall                                                read return
+ XX    syscall           vmlinux                              read entry
+ XX    syscall           vmlinux                              read return
 -- @@stderr --
 dtrace: failed to match ::fight:: No probe matches description
 dtrace: failed to match ::fight:: No probe matches description
