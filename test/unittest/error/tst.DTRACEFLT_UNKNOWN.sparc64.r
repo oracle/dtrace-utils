5a198ed9daab8f3f3eb56ad2bdf2b14eea015648
Kris Van Hees <kris.van.hees@oracle.com>
2014-09-24 21:17:37 -0400

    Add sparc64-specific expected output for tst.DTRACEFLT_UNKNOWN.d.
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/error/tst.DTRACEFLT_UNKNOWN.sparc64.r
@@ -0,0 +1,5 @@
+The arguments are 2 2 4 1 0
+The value of arg4 = 0
+
+-- @@stderr --
+dtrace: error on enabled probe ID 2 (ID 1: dtrace:::BEGIN): invalid address ({ptr}) in action #2 at DIF offset 4
