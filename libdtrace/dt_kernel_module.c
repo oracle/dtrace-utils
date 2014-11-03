7f80374e566fd89d4d52c32ed9b9d7ec9a1ec84d
Nick Alcock <nick.alcock@oracle.com>
2014-11-03 18:32:17 +0000

    No longer use fts() to find kernel module name->path mappings.
    This is deprecated, doesn't work with large-file APIs, and is generally
    considered bad.  Of course, its replaement -- nftw() -- has other problems,
    notably the 'qsort() bug' that it calls a function of your choice to do the work
    of iteration but provides no way to pass an arbitrary pointer to it, so you
    can't get any state in (like the dtp pointer).
    
    But of course we don't need to update kernel module locations often: we just
    want to protect against some hypothetical multithreaded libdtrace consumer
    calling dt_module_lookup_by_name() for the first time simultaneously in multiple
    threads, and then we can use a static global to pass the necessary state.  So
    add a static mutex to do that.
    
    This also lets us fix up the memory allocation used by the module-iteration
    path, avoiding a static MAXPATH-sized buffer for the module path in favour of
    the dynamic allocation we're doing everywhere else anyway.

--- libdtrace/dt_kernel_module.c
+++ libdtrace/dt_kernel_module.c
@@ -19,44 +19,63 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2009, 2011, 2012, 2013 Oracle, Inc.  All rights reserved.
+ * Copyright 2009, 2011, 2012, 2013, 2014 Oracle, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#include <assert.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fts.h>
-#include <string.h>
-#include <dt_kernel_module.h>
-#include <dt_string.h>
 
 /*
  * Kernel module list management.  We must maintain bindings from
  * name->filesystem path for all the current kernel's modules, since the system
  * maintains no such list and all mechanisms other than find(1)-analogues have
  * been deprecated or removed in kmod.
+ */
+
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <ftw.h>
+#include <pthread.h>
+#include <string.h>
+
+#include <dt_kernel_module.h>
+#include <dt_string.h>
+#include <port.h>
+
+static pthread_mutex_t kern_path_update_lock = PTHREAD_MUTEX_INITIALIZER;
+static dtrace_hdl_t *locked_dtp;
+
+static int dt_kern_path_update_one_entry(const char *fpath, const struct stat *sb,
+    int typeflag, struct FTW *ftwbuf);
+
+/*
+ * On successful return, name and path become owned by dt_kern_path_create()
+ * (and may be freed immediately).
  *
  * Note: this code is used by ctf_module_dump as well.
  */
 
 dt_kern_path_t *
-dt_kern_path_create(dtrace_hdl_t *dtp, const char *name, const char *path)
+dt_kern_path_create(dtrace_hdl_t *dtp, char *name, char *path)
 {
 	uint_t h = dt_strtab_hash(name, NULL) % dtp->dt_kernpathbuckets;
 	dt_kern_path_t *dkpp;
 
 	for (dkpp = dtp->dt_kernpaths[h]; dkpp != NULL; dkpp = dkpp->dkp_next) {
-		if (strcmp(dkpp->dkp_name, name) == 0)
+		if (strcmp(dkpp->dkp_name, name) == 0) {
+			free(name);
+			free(path);
 			return (dkpp);
+		}
 	}
 
 	if ((dkpp = malloc(sizeof (dt_kern_path_t))) == NULL)
 		return (NULL); /* caller must handle allocation failure */
 
+	dt_dprintf("Adding %s -> %s\n", name, path);
+
 	bzero(dkpp, sizeof (dt_kern_path_t));
-	dkpp->dkp_name = strdup(name);
-	dkpp->dkp_path = strdup(path);
+	dkpp->dkp_name = name;
+	dkpp->dkp_path = path;			/* strdup()ped by our caller */
 	dt_list_append(&dtp->dt_kernpathlist, dkpp);
 	dkpp->dkp_next = dtp->dt_kernpaths[h];
 	dtp->dt_kernpaths[h] = dkpp;
@@ -92,71 +111,66 @@ dt_kern_path_destroy(dtrace_hdl_t *dtp, dt_kern_path_t *dkpp)
 
 /*
  * Construct the mapping of kernel module name -> path for all modules.
+ *
+ * Must be called under the kern_path_update_lock.
  */
 int
 dt_kern_path_update(dtrace_hdl_t *dtp)
 {
-	FTS *ftp;
-	FTSENT *module;
-	char *modpathargs[2] = { dtp->dt_module_path, NULL };
-
-	ftp = fts_open(modpathargs, FTS_PHYSICAL | FTS_NOCHDIR, NULL);
-
-	if (ftp == NULL)
-		return errno;
-
-	while ((module = fts_read(ftp)) != NULL) {
-		int fterrno;
-		char *suffix;
-		char modname[PATH_MAX];
-
-		switch (module->fts_info) {
-		case FTS_DNR:
-		case FTS_ERR:
-			fterrno = module->fts_errno;
-			fts_close(ftp);
-			dt_dprintf("Failed to read kernel module hierarchy "
-			    "rooted at %s: %s\n", dtp->dt_module_path,
-			    strerror(fterrno));
-
-			return fterrno;
-		case FTS_F:
-			break;
-		default:
-			continue;
-		}
-
-		suffix = strrstr(module->fts_name, ".ko");
-		if ((suffix == NULL) || (suffix[3] != '\0'))
-			continue;
-
-		strcpy(modname, module->fts_name);
-		suffix = strrstr(modname, ".ko");
-		suffix[0] = '\0';
-
-		if (dt_kern_path_create(dtp, modname, module->fts_path) == NULL) {
-			fts_close(ftp);
-			return EDT_NOMEM;
-		}
-	}
+	assert(MUTEX_HELD(&kern_path_update_lock));
 
 	/*
-	 * fts_read() is unusual in that it sets errno to 0 on a successful
-	 * return.
+	 * 10 is a totally arbitrary number way below any likely useful limit on
+	 * the fd count.  Anyone setting ulimits below this deserves what they
+	 * get.
 	 */
-	if (errno != 0) {
-		int error;
 
-		fts_close(ftp);
-		error = errno;
-		dt_dprintf("Failed to close search for kernel module hierarchy "
-		    "rooted at %s/kernel: %s\n", dtp->dt_module_path,
-		    strerror(error));
+	locked_dtp = dtp;
+	return nftw(dtp->dt_module_path, dt_kern_path_update_one_entry,
+	    10, FTW_PHYS);
+}
 
-		return error;
+/*
+ * The guts of dt_kern_path_update().
+ */
+static int
+dt_kern_path_update_one_entry(const char *fpath, const struct stat *sb,
+    int typeflag, struct FTW *ftwbuf)
+{
+	char *suffix;
+	char *modname;
+	char *modpath;
+
+	/*
+	 * Fail if whole directories can't be read, since this means we'll miss
+	 * modules.
+	 */
+	switch (typeflag) {
+	case FTW_DNR:
+		return -EPERM;
+	case FTW_F:
+		break;
+	/*
+	 * Unstattable, directory, etc.
+	 */
+	default:
+		return 0;
 	}
 
-	fts_close(ftp);
+	suffix = strrstr(&fpath[ftwbuf->base], ".ko");
+	if ((suffix == NULL) || (suffix[3] != '\0'))
+		return 0;
+
+	modpath = strdup(fpath);
+	modname = strndup(&fpath[ftwbuf->base],
+	    strlen(&fpath[ftwbuf->base]) - 3);
+
+	if (dt_kern_path_create(locked_dtp, modname, modpath) == NULL) {
+		free(modname);
+		free(modpath);
+		return EDT_NOMEM;
+	}
+
 	return 0;
 }
 
@@ -167,13 +181,20 @@ dt_kern_path_lookup_by_name(dtrace_hdl_t *dtp, const char *name)
 	dt_kern_path_t *dkpp;
 
 	if (dtp->dt_nkernpaths == 0) {
-		int dterrno = dt_kern_path_update(dtp);
+		int dterrno;
 
-		dt_dprintf("Initialized %i kernel module paths, errno %i\n",
-		    dtp->dt_nkernpaths, dterrno);
+		pthread_mutex_lock(&kern_path_update_lock);
+		dterrno = dt_kern_path_update(dtp);
+		pthread_mutex_unlock(&kern_path_update_lock);
 
-		if (dterrno != 0)
+		if (dterrno != 0) {
+			dt_dprintf("Error initializing kernel module paths: "
+			    "%s\n", dtrace_errmsg(dtp, dterrno));
 			return (NULL);
+		}
+
+		dt_dprintf("Initialized %i kernel module paths\n",
+		    dtp->dt_nkernpaths);
 	}
 
 	for (dkpp = dtp->dt_kernpaths[h]; dkpp != NULL; dkpp = dkpp->dkp_next) {
@@ -188,8 +209,7 @@ dt_kern_path_lookup_by_name(dtrace_hdl_t *dtp, const char *name)
  * Public API wrappers.
  */
 dt_kern_path_t *
-dtrace__internal_kern_path_create(dtrace_hdl_t *dtp, const char *name,
-                                 const char *path)
+dtrace__internal_kern_path_create(dtrace_hdl_t *dtp, char *name, char *path)
     __attribute__((alias("dt_kern_path_create")));
 
 void
--- libdtrace/dt_kernel_module.h
+++ libdtrace/dt_kernel_module.h
@@ -20,7 +20,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2012, 2013 Oracle, Inc.  All rights reserved.
+ * Copyright 2012, 2013, 2014 Oracle, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -29,8 +29,8 @@
 
 #include <dt_impl.h>
 
-extern dt_kern_path_t *dt_kern_path_create(dtrace_hdl_t *dtp,
-    const char *name, const char *path);
+extern dt_kern_path_t *dt_kern_path_create(dtrace_hdl_t *dtp, char *name,
+    char *path);
 extern int dt_kern_path_update(dtrace_hdl_t *dtp);
 extern dt_kern_path_t *dt_kern_path_lookup_by_name(dtrace_hdl_t *dtp,
     const char *name);
@@ -45,8 +45,7 @@ extern void dt_kern_path_destroy(dtrace_hdl_t *dtp, dt_kern_path_t *dkpp);
  */
 
 extern dt_kern_path_t *
-dtrace__internal_kern_path_create(dtrace_hdl_t *dtp, const char *name,
-    const char *path);
+dtrace__internal_kern_path_create(dtrace_hdl_t *dtp, char *name, char *path);
 
 extern void
 dtrace__internal_kern_path_destroy(dtrace_hdl_t *dtp, dt_kern_path_t *dkpp);
