774eb3e5a3fd4f8fbff6e23e8ed12749858494cf
Kris Van Hees <kris.van.hees@oracle.com>
2015-06-23 11:08:46 -0400

    Ensure sigaltstack tracing does not crash the system
    Due to an inconsistency between the kernel and modules code, tracing
    the sigaltstack syscall would crash the system if a userspace process
    would actually use that system call.
    
    This test ensure that tracing sigaltstack works by using a program
    that is known to use that system call (vim).
    
    Orabug: 21304183 21306269
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/regression/tst.sigaltstack.d
@@ -0,0 +1,50 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2015 Oracle, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#pragma D option destructive
+#pragma D option quiet
+
+/*
+ * Ensure that tracing sigaltstack syscalls works.
+ */
+BEGIN
+{
+	/* Timeout after 2 seconds */
+	timeout = timestamp + 2000000000;
+	system("vim -c :q tmpfile >/dev/null 2>&1 &");
+}
+
+syscall::sigaltstack:entry
+{
+	exit(0);
+}
+
+profile:::tick-100ms
+/timestamp > timeout/
+{
+	trace("test timed out");
+	exit(1);
+}
