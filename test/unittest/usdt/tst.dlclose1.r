a328992606c7bd47dd7dd0e073b23245f5316bd8
Nick Alcock <nick.alcock@oracle.com>
2015-10-06 22:03:22 +0100

    Regenerate tst.dlclose1.sh expected results.
    Splitting stdout and stderr changes them.

--- test/unittest/usdt/tst.dlclose1.r
+++ test/unittest/usdt/tst.dlclose1.r
@@ -1,3 +1,4 @@
-dtrace: failed to match test_prov*:::: No probe matches description
    ID   PROVIDER            MODULE                          FUNCTION NAME
 
+-- @@stderr --
+dtrace: failed to match test_prov*:::: No probe matches description
