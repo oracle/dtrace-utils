ff3a79d805e5bed2292c7789a9485816c0b3aec4
Nick Alcock <nick.alcock@oracle.com>
2016-05-10 17:32:46 +0100

    libproc: add pldd test results for sparc64.
    The sparc64 pldd libproc test results are expected to differ from
    x86: the dynamic linker name differs, and ld.so's behaviour is
    subtly different too (there is no entry for ld.so itself in the
    link map).
    
    So we need new results for it.

---
+++ test/internals/libproc/tst.plddCreate.sparc64.r
@@ -0,0 +1,4 @@
+: dyn {ptr}, bias {ptr}, LMID 0: symbol search path:  (, /lib64/libc.so.6, /lib64/ld-linux.so.2)
+/lib64/libc.so.6: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib64/libc.so.6, /lib64/ld-linux.so.2)
+/lib64/ld-linux.so.2: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib64/libc.so.6, /lib64/ld-linux.so.2)
+3 libs seen.
---
+++ test/internals/libproc/tst.plddCreatePIE32.sparc64.r
@@ -0,0 +1,4 @@
+: dyn {ptr}, bias {ptr}, LMID 0: symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
+/lib/libc.so.6: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
+/lib/ld-linux.so.2: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
+3 libs seen.
