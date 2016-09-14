7e29f21a747adf940f16dcf0835e927d0031c586
Nick Alcock <nick.alcock@oracle.com>
2016-09-14 12:13:39 +0100

    tests: new tests for translator argument elision and typed perf probes

---
+++ test/unittest/sdt/tst.translation-elided-arg.r
@@ -0,0 +1,45 @@
+                   FUNCTION:NAME
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+dt_test_ioctl:sdt-test-ioctl-file 
+dt_test_ioctl:sdt-test-ioctl-file         0
+                exit_group:entry 
+
+-- @@stderr --
+dtrace: script 'test/unittest/sdt/tst.translation-elided-arg.d' matched 3 probes
