024fad4231cc6e536ecd97098207920224533404
Nick Alcock <nick.alcock@oracle.com>
2016-09-16 10:41:39 +0100

    tests: adjust for new probes
    There are yet more new probes in dt_test.

--- test/unittest/dtrace-util/tst.ListProbesModuleSDT.r
+++ test/unittest/dtrace-util/tst.ListProbesModuleSDT.r
@@ -2,9 +2,25 @@
  XX        sdt           dt_test                               foo sdt-test2
  XX        sdt           dt_test                     dt_test_ioctl sdt-test
  XX        sdt           dt_test                     dt_test_ioctl sdt-test-ioctl-file
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg1
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg2
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg3
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg4
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg5
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg6
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg7
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg8
  XX    dt_test           dt_test                                   test
    ID   PROVIDER            MODULE                          FUNCTION NAME
  XX        sdt           dt_test                               foo sdt-test2
  XX        sdt           dt_test                     dt_test_ioctl sdt-test
  XX        sdt           dt_test                     dt_test_ioctl sdt-test-ioctl-file
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg1
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg2
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg3
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg4
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg5
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg6
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg7
+ XX        sdt           dt_test                     dt_test_ioctl sdt-test-arg8
  XX    dt_test           dt_test                                   test
