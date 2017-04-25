6151add313992ddc53874df198bfcc77f30ed79d
Nick Alcock <nick.alcock@oracle.com>
2017-04-25 15:44:40 +0100

    tests: filter the vDSO out of libproc create/grab pldd tests
    These tests test libproc's phdr iteration and library search-path lookup
    functions by running an enhanced version of ldd(1) on a test binary
    (enhanced in that it can print the search path in force for each shared object,
    which ldd cannot).  However, this also prints out the internal entry for the
    vDSO, which is problematic because the vDSO is not present on all systems
    (in particular, it is not present on upstream SPARC kernels without the UEK
    patches installed).  The lack of the vDSO does not mean that libproc is
    broken, so filter it out so its presence or absence doesn't affect us.

--- test/internals/libproc/tst.plddGrab32.r
+++ test/internals/libproc/tst.plddGrab32.r
@@ -1,5 +1,4 @@
 : dyn {ptr}, bias {ptr}, LMID 0: symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
-: dyn {ptr}, bias {ptr}, LMID 0: symbol search path:  ()
 /lib/libc.so.6: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
 /lib/ld-linux.so.2: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib/libc.so.6, /lib/ld-linux.so.2)
-4 libs seen.
+3 libs seen.
--- test/triggers/libproc-pldd.c
+++ test/triggers/libproc-pldd.c
@@ -54,6 +54,12 @@ print_ldd(const rd_loadobj_t *loadobj, size_t num, void *state)
 		return (0);
 	}
 
+	/*
+	 * No name, no search path: vDSO, we don't care about it.
+	 */
+	if (buf[0] == '\0' && loadobj->rl_nscopes == 1)
+		return (1);
+
 	printf("%s: dyn 0x%lx, bias 0x%lx, LMID %li: %s (", buf, loadobj->rl_dyn,
 	    loadobj->rl_diff_addr, loadobj->rl_lmident, loadobj->rl_default_scope ?
 	    "inherited symbol search path: ": "symbol search path: ");
@@ -87,6 +93,12 @@ note_ldd(const rd_loadobj_t *loadobj, size_t num, void *state)
 		    loadobj->rl_nameaddr);
 		return (0);
 	}
+
+	/*
+	 * No name, no search path: vDSO, we don't care about it.
+	 */
+	if (buf[0] == '\0' && loadobj->rl_nscopes == 1)
+		return (1);
 
 	libs_seen++;
 	return (1);
