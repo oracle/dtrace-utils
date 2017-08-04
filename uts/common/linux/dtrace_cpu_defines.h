6926791efce243da48ae3e61f6da56a650e2daf3
Nick Alcock <nick.alcock@oracle.com>
2017-08-04 14:26:46 +0100

    dtrace_cpu_defines.h: Make a standalone header.
    This uses uint32_t, so it should include the header that defines that
    type.

--- uts/common/linux/dtrace_cpu_defines.h
+++ uts/common/linux/dtrace_cpu_defines.h
@@ -8,7 +8,9 @@
 #ifndef _LINUX_DTRACE_CPU_DEFINES_H_
 #define _LINUX_DTRACE_CPU_DEFINES_H_
 
-typedef int32_t		processorid_t;
+#include <stdint.h>
+
+typedef uint32_t	processorid_t;
 typedef uint32_t	psetid_t;
 typedef uint32_t	chipid_t;
 typedef uint32_t	lgrp_id_t;
