29f42d0cdee0d7b941a769e7c939a5c6643975aa
Nick Alcock <nick.alcock@oracle.com>
2017-09-01 10:49:52 +0100

    lexer: allow keywords after structure member dereference operators
    We want to allow references to the numerous things named 'self' in the
    Linux kernel, even though there is a D keyword of the same name.  There
    is really no justification for prohibiting access to any keyword at all
    in this position: nothing is syntactically valid after a -> or . other
    than an identifier or comment either (the parser contains no rules for
    other cases, and the code generator contains an assertion validating the
    same thing).
    
    The fix is simple: add a new start condition that indicates to the lexer
    that we are inside a place where only an identifier (or comment) is
    permitted, then conditionalize identifier matches on this condition and
    reset it back to its original condition again.  We are lucky in that all
    such places are in condition S0, so we don't need to specially track the
    previous lexer condition (since the only thing that tracks such things
    is already in use for comments.)
    
    This is not terribly elegant, but it works, it's no uglier than the
    things the lexer is doing anyway, and we are unlikely to need to
    extend it in future, since the chance of extra things being permitted,
    C++-style, after . or -> is remote.
    
    Orabug: 26518086

---
+++ test/unittest/lexer/tst.keyword-member.d
@@ -0,0 +1,26 @@
+/*
+ * Oracle Linux DTrace.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/*
+ * ASSERTION:
+ *	A member named after a D keyword can be referenced, both
+ *      after -> and after ., even with comments interjected.
+ */
+
+BEGIN
+{
+	printf("%p\n", &`cgrp_dfl_root.cgrp.self);
+	printf("%p\n", &`cgrp_dfl_root.cgrp.self.cgroup->self);
+	printf("%p\n", `cgrp_dfl_root.cgrp.self.cgroup->self.cgroup);
+	printf("%p\n", &`cgrp_dfl_root.cgrp./* foo */self.cgroup->self);
+	printf("%p\n", `cgrp_dfl_root.cgrp.self.cgroup->/* foo */self.cgroup);
+	printf("%p\n", &`cgrp_dfl_root.cgrp.self/* foo */.cgroup->self);
+	printf("%p\n", `cgrp_dfl_root.cgrp.self.cgroup->self/* foo */.cgroup);
+
+	exit(0);
+}
+
