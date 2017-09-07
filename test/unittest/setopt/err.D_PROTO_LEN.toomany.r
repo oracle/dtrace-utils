cec5571ec774eeca33bc6f77db08e2b03344d1a5
Eugene Loh <eugene.loh@oracle.com>
2017-09-07 11:06:13 -0700

    fix error messages when wrong number of args and expected number is not unique
    In libdtrace/dt_ident.c dt_idcook_sign(), we report when the number of arguments
    is unexpected.  The logic needed cleaning up.  E.g. we might report an error
    because 4 arguments were present but at least 2 were expected.
    
    Orabug: 26402731
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle>
    Reviewed-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>

---
+++ test/unittest/setopt/err.D_PROTO_LEN.toomany.r
@@ -0,0 +1,2 @@
+-- @@stderr --
+dtrace: failed to compile script test/unittest/setopt/err.D_PROTO_LEN.toomany.d: [D_PROTO_LEN] line 18: setopt( ) prototype mismatch: 3 args passed, at most 2 expected
