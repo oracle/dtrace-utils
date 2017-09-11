b60ad2a6e7e9354a4c5f1e49891b95ae808cf203
Eugene Loh <eugene.loh@oracle.com>
2017-09-11 16:18:09 -0700

    fix error messages when wrong number of args and expected number is not unique
    Fix copyright and license notices.
    
    Orabug: 26402731
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle>
    Reviewed-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>

--- test/unittest/printf/err.D_PROTO_LEN.toofew.r
+++ test/unittest/printf/err.D_PROTO_LEN.toofew.r
@@ -1,3 +1,2 @@
 -- @@stderr --
 dtrace: failed to compile script test/unittest/printf/err.D_PROTO_LEN.toofew.d: [D_PROTO_LEN] line 18: printf( ) prototype mismatch: 0 args passed, at least 1 expected
-
--- test/unittest/setopt/err.D_PROTO_LEN.toomany.r
+++ test/unittest/setopt/err.D_PROTO_LEN.toomany.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/setopt/err.D_PROTO_LEN.toomany.d: [D_PROTO_LEN] line 39: setopt( ) prototype mismatch: 3 args passed, at most 2 expected
+dtrace: failed to compile script test/unittest/setopt/err.D_PROTO_LEN.toomany.d: [D_PROTO_LEN] line 18: setopt( ) prototype mismatch: 3 args passed, at most 2 expected
