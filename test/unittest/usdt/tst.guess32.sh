bc53b7d6adc4a0fe8de7939e081e62a9a2c68231
Nick Alcock <nick.alcock@oracle.com>
2017-11-28 21:16:56 +0000

    tests: tst.guess32.sh: 32-on-64 usdt support has worked for ages.
    This should not be XFAIL.

--- test/unittest/usdt/tst.guess32.sh
+++ test/unittest/usdt/tst.guess32.sh
@@ -4,8 +4,6 @@
 # Copyright (c) 2006, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
-#
-# @@xfail: 32-on-64 support is still under development
 
 if [ $# != 1 ]; then
 	echo expected one argument: '<'dtrace-path'>'
