d9158b26e110ddff4ef7c9b59da554e0e7fd1ad4
Nick Alcock <nick.alcock@oracle.com>
2018-01-29 19:18:54 +0000

    test: adjust err.unloaded_var.d again.
    The non-loaded module we were using before is no longer built at all.
    Switch to another one that is probably not often loaded on DTrace
    test systems (but is always built).

--- test/unittest/types/err.unloaded_var.r
+++ test/unittest/types/err.unloaded_var.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/types/err.unloaded_var.d: line 21: failed to resolve hysdn`cardmax: Module is no longer loaded
+dtrace: failed to compile script test/unittest/types/err.unloaded_var.d: line 21: failed to resolve gfs2`gfs2_control_wq: Module is no longer loaded
