537e00636d7d0e336819018db7d4ee17ad7a66cd
Eugene Loh <eugene.loh@oracle.com>
2018-02-06 14:50:53 -0800

    Improve testing of symbol support
    DTrace supports lookup by address of symbol or module and lookup by name
    of symbol.  The support was rife with bugs (fixed by recent putbacks)
    and was only lightly tested.  Here we beef up testing.
    
    tst.aggmod_full.sh had been tagged xfail.  The underlying bugs have
    been fixed.  So we remove the xfail tag (along with explanation).
    We also omit symbols of types 'a' and 'A' in light of recent changes.
    The test attempts to check all symbols in /proc/kallmodsyms; but if
    the timeout is imminent, settle for partial coverage (subject to some
    minimum).
    
    With the full test working, we eliminate the specialized tst.aggmod.sh.
    
    tst.symbols.c is added.
    
    tst.merge_ranges_bug25767469.c is expanded to check whether address
    ranges from different modules overlap.  There is also some
    refactoring to improve maintainability.
    
    Orabug: 27214992
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>

--- test/unittest/consumer/tst.merge_ranges_bug25767469.c
+++ test/unittest/consumer/tst.merge_ranges_bug25767469.c
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -11,53 +11,103 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <dtrace.h>
+
+typedef struct myrange{
+	long unsigned addr;
+	long unsigned size;
+	char *name;
+	char type;
+} myrange_t;
+
+myrange_t *ranges;
+int maxnranges, nranges;
+
+int mycompare(const void *ap, const void *bp)
+{
+	myrange_t *a = (myrange_t *) ap;
+	myrange_t *b = (myrange_t *) bp;
+	if (a->addr < b->addr)
+		return -1;
+	if (a->addr > b->addr)
+		return +1;
+	if (a->size < b->size)
+		return -1;
+	if (a->size > b->size)
+		return +1;
+	if (a->type < b->type)
+		return +1;
+	if (a->type > b->type)
+		return -1;
+	return strcmp(a->name, b->name);
+}
+
+void print_range(myrange_t *r)
+{
+	printf(" %c range %s %lx-%lx",
+	    r->type, r->name, r->addr, r->addr + r->size);
+}
 
 int nerrors = 0;
 
+/*
+ * Look at the address ranges for a particular module name and type
+ * (text 't' or data 'd').  A built-in module might not have a single
+ * text or data segment.
+ */
+
+void check_module_ranges
+(size_t n, dtrace_addr_range_t *addrs, char *name, char type)
+{
+	int i;
+	/*
+	 * Check for a gap between consecutive ranges.
+	 * The check ensures:
+	 *   - ranges are in order
+	 *   - ranges do not overlap or abut
+	 *         (otherwise they should have been coalesced)
+	 */
+	for (i = 1; i < n; i++) {
+		if ( addrs[i-1].dar_va
+		    + addrs[i-1].dar_size
+		    >= addrs[i].dar_va ) {
+			printf("ERROR: %s %c range %lx-%lx"
+			    " overlaps or abuts %lx-%lx\n", name, type,
+			    addrs[i-1].dar_va,
+			    addrs[i-1].dar_va + addrs[i-1].dar_size,
+			    addrs[i].dar_va,
+			    addrs[i].dar_va + addrs[i].dar_size);
+			nerrors++;
+		}
+	}
+
+	/*
+	 * Record ranges so that we can later check
+	 * all ranges from all modules against each other.
+	 */
+	for (i = 0; i < n; i++) {
+		if (nranges == maxnranges) {
+			maxnranges *= 4;
+			ranges = (myrange_t *) realloc(ranges,
+			    maxnranges * sizeof (myrange_t));
+		}
+		ranges[nranges].addr = addrs[i].dar_va;
+		ranges[nranges].size = addrs[i].dar_size;
+		ranges[nranges].name = strdup(name);
+		ranges[nranges].type = type;
+		nranges++;
+	}
+}
+
 int mod_callback(dtrace_hdl_t * h, const dtrace_objinfo_t * d, void * arg) {
 	if (arg) {
-		/*
-		 * When arg!=0, check for a gap between consecutive ranges.
-		 * The check ensures:
-		 *   - ranges are in order
-		 *   - ranges do not overlap or abut
-		 */
-		int i;
-
-		/* dta_text_addrs */
-		for (i = 1; i < d->dto_text_addrs_size; i++) {
-			if ( d->dto_text_addrs[i-1].dar_va
-			    + d->dto_text_addrs[i-1].dar_size
-			    >= d->dto_text_addrs[i  ].dar_va ) {
-				printf("ERROR: text range %lx-%lx"
-				    " overlaps or abuts %lx-%lx\n",
-				    d->dto_text_addrs[i-1].dar_va,
-				    d->dto_text_addrs[i-1].dar_va
-				    + d->dto_text_addrs[i-1].dar_size,
-				    d->dto_text_addrs[i  ].dar_va,
-				    d->dto_text_addrs[i  ].dar_va
-				    + d->dto_text_addrs[i  ].dar_size);
-				nerrors++;
-			}
-		}
-
-		/* dto_data_addrs */
-		for (i = 1; i < d->dto_data_addrs_size; i++) {
-			if ( d->dto_data_addrs[i-1].dar_va
-			    + d->dto_data_addrs[i-1].dar_size
-			    >= d->dto_data_addrs[i  ].dar_va ) {
-				printf("ERROR: data range %lx-%lx"
-				    " overlaps or abuts %lx-%lx\n",
-				    d->dto_data_addrs[i-1].dar_va,
-				    d->dto_data_addrs[i-1].dar_va
-				    + d->dto_data_addrs[i-1].dar_size,
-				    d->dto_data_addrs[i  ].dar_va,
-				    d->dto_data_addrs[i  ].dar_va
-				    + d->dto_data_addrs[i  ].dar_size);
-				nerrors++;
-			}
-		}
+		/* when arg!=0, check the module ranges */
+		check_module_ranges(d->dto_text_addrs_size,
+		    d->dto_text_addrs, d->dto_name, 't');
+		check_module_ranges(d->dto_data_addrs_size,
+		    d->dto_data_addrs, d->dto_name, 'd');
 	} else {
 		/* when arg==0; dry run: look up info to force filename to load */
 		GElf_Sym symp;
@@ -76,13 +126,20 @@ int mod_callback(dtrace_hdl_t * h, const dtrace_objinfo_t * d, void * arg) {
 
 int main(int argc, char **argv)
 {
-	int err;
+	int i, err;
 	dtrace_hdl_t *h = dtrace_open(DTRACE_VERSION, 0, &err);
 	if (h == NULL) {
 		printf("ERROR: dtrace_open %d |%s|\n",
 		    err, dtrace_errmsg(h, err));
 		return 1;
 	}
+
+	/*
+	 * initialize list of ranges
+	 */
+	maxnranges = 4096;
+	nranges = 0;
+	ranges = (myrange_t *) malloc(maxnranges * sizeof (myrange_t));
 
 	/*
 	 * Some info is populated lazily.
@@ -94,6 +151,25 @@ int main(int argc, char **argv)
 	dtrace_object_iter(h, mod_callback, (void *)1);
 
 	dtrace_close(h);
+
+	printf("%d preliminary errors\n", nerrors);
+
+	/*
+	 * Sort the ranges from among all modules and check for overlaps.
+	 */
+	qsort(ranges, nranges, sizeof (myrange_t), &mycompare);
+	for (i = 1; i < nranges; i++) {
+		if ( ranges[i-1].addr + ranges[i-1].size
+		    > ranges[i].addr ) {
+			printf("ERROR:");
+			print_range(&ranges[i-1]);
+			printf(" overlaps");
+			print_range(&ranges[i]);
+			printf("\n");
+			nerrors++;
+		}
+	}
+
 	printf("test tst.merge_ranges_bug25767469.c had %d errors\n", nerrors);
 	return (nerrors != 0);
 }
