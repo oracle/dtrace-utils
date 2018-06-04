03e5e8521177823ee7b01f9a6cab0a7d7b32b32d
Kris Van Hees <kris.van.hees@oracle.com>
2018-06-04 16:16:49 +0000

    Enable tests for ustack() on arm64
    Orabug: 28446888
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/regression/tst.unload-reload-oops.aarch64.x
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ `uname -m` = "aarch64" ] && exit 2
-exit 0
--- test/unittest/ustack/tst.depth.sh
+++ test/unittest/ustack/tst.depth.sh
@@ -4,8 +4,6 @@
 # Copyright (c) 2006, 2018, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
-#
-# @@xfail: inconsistent results
 
 if [ $# != 1 ]; then
 	echo expected one argument: '<'dtrace-path'>'
