13554f782a38fa7bb3c0a177d78b00c4b9a098db
Nick Alcock <nick.alcock@oracle.com>
2018-12-14 08:26:36 +0000

    tests: expected results for internals pldd tests for AArch64
    These tests have output dependent on the dynamic linker name.

---
+++ test/internals/libproc/tst.plddGrabPIE.aarch64.r
@@ -0,0 +1,4 @@
+: dyn {ptr}, bias {ptr}, LMID 0: symbol search path:  (, /lib64/libc.so.6, /lib/ld-linux-aarch64.so.1)
+/lib64/libc.so.6: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib64/libc.so.6, /lib/ld-linux-aarch64.so.1)
+/lib/ld-linux-aarch64.so.1: dyn {ptr}, bias {ptr}, LMID 0: inherited symbol search path:  (, /lib64/libc.so.6, /lib/ld-linux-aarch64.so.1)
+3 libs seen.
