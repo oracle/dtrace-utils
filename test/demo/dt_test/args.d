2b233913b4b383325de4eee289e3f19dfac21882
Nick Alcock <nick.alcock@oracle.com>
2018-12-14 15:24:51 +0000

    tests: demo/dt_test/args.d works, but with different args per-arch
    Add appropriate ifdeffery.

--- test/demo/dt_test/args.d
+++ test/demo/dt_test/args.d
@@ -1,11 +1,12 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 
 /* @@trigger: testprobe */
+/* @@runtest-opts: -C */
 
 /*
  * Ensure that arguments to direct-call probes can be retrieved both from
@@ -14,7 +15,12 @@
 dt_test:::test
 /i++ == 0/
 {
+#ifndef __aarch64__
 	printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",
 	       arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+#else
+	printf("%d, %d, %d, %d, %d, %d, %d\n",
+	       arg0, arg1, arg2, arg3, arg4, arg5, arg6);
+#endif
 	exit(0);
 }
