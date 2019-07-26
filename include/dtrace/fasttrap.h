55cc3278e49e9ace307b9fddd1d52dea4d0477ab
Kris Van Hees <kris.van.hees@oracle.com>
2019-07-26 19:33:37 -0400

    Update copyright statements.

--- include/dtrace/fasttrap.h
+++ include/dtrace/fasttrap.h
@@ -2,7 +2,7 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  *
- * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2019, Oracle and/or its affiliates. All rights reserved.
  */
 
 /*
--- libdtrace/dt_parser.h
+++ libdtrace/dt_parser.h
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2019, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
