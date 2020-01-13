b51f901641da80f32c7362176a2d5490d5aaf632
Eugene Loh <eugene.loh@oracle.com>
2020-01-13 15:47:57 -0500

    Clean up register-set management.
    Commit 0ae0393d000767685355fd221d6df4805b32f26b reverses the
    search order for unused registers.  Rather than marching from
    %r0 up, it starts at the top register and moves down, testing
    whether the register number is >=0.  Since this number is
    declared an unsigned integer, however, the test always passes.
    The code never realizes that all registers have been exhausted.
    
    But this register management has always been a little messy.
    It relies on include/sys/bitmap.h, which supports arbitrary-size
    bit maps.  This is arguably overkill;  eBPF supports fewer than
    a dozen registers, while a single word can accommodate 64 bits.
    Nevertheless, using bitmap.h is easy enough and allows headroom
    for eBPF growing its number of registers.  But dt_regset.* uses
    private information about the bitmaps, violating the
    architectural boundary set by bitmap.h and complicating the code.
    
    Further, it introduces an optimization that is both pointless and
    implemented incorrectly.  In function dt_regset_alloc(), we have:
    
    	for (wx = maxw; wx >= maxw; wx--) {
    		if (drp->dr_bitmap[wx] != ~0UL)
    			break;
    	}
    
    The optimization is pointless:  it is trying to find quickly a
    word in the bitmask that has at least one unset bit, but there is
    only one word in the bitmask (until the day DTrace wants to support
    over 64 registers).  The test is also incorrect since the "last word"
    -- in our case, the only word -- is a partial word and will never be
    ~0UL, even if all its bits are set.
    
    This commit fixes and cleans up the code.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- libdtrace/dt_regset.c
+++ libdtrace/dt_regset.c
@@ -17,21 +17,21 @@
 dt_regset_t *
 dt_regset_create(ulong_t size)
 {
-	ulong_t n = BT_BITOUL(size + 1); /* + 1 for %r0 */
 	dt_regset_t *drp = malloc(sizeof (dt_regset_t));
 
 	if (drp == NULL)
 		return (NULL);
 
-	drp->dr_bitmap = malloc(sizeof (ulong_t) * n);
-	drp->dr_size = size + 1;
+	size++;  /* for %r0 */
 
+	drp->dr_size = size;
+	drp->dr_bitmap = malloc(BT_SIZEOFMAP(size));
 	if (drp->dr_bitmap == NULL) {
-		dt_regset_destroy(drp);
+		free(drp);
 		return (NULL);
 	}
 
-	memset(drp->dr_bitmap, 0, sizeof (ulong_t) * n);
+	dt_regset_reset(drp);
 	return (drp);
 }
 
@@ -45,36 +45,20 @@ dt_regset_destroy(dt_regset_t *drp)
 void
 dt_regset_reset(dt_regset_t *drp)
 {
-	memset(drp->dr_bitmap, 0, sizeof (ulong_t) * BT_BITOUL(drp->dr_size));
+	memset(drp->dr_bitmap, 0, BT_SIZEOFMAP(drp->dr_size));
 }
 
 int
 dt_regset_alloc(dt_regset_t *drp)
 {
-	ulong_t nbits = drp->dr_size - 1;
-	ulong_t maxw = nbits >> BT_ULSHIFT;
-	ulong_t wx;
+	int reg;
 
-	for (wx = maxw; wx >= maxw; wx--) {
-		if (drp->dr_bitmap[wx] != ~0UL)
-			break;
-	}
-
-	if (wx >= 0) {
-		ulong_t maxb = (wx == maxw) ? nbits & BT_ULMASK : BT_NBIPUL - 1;
-		ulong_t word = drp->dr_bitmap[wx];
-		ulong_t bit, bx;
-		int reg;
-
-		for (bit = 1 << maxb, bx = maxb; bx >= 0; bx--, bit >>= 1) {
-			if ((word & bit) == 0) {
-				reg = (int)((wx << BT_ULSHIFT) | bx);
-				BT_SET(drp->dr_bitmap, reg);
-				return (reg);
-			}
+	for (reg = drp->dr_size - 1; reg >= 0; reg--) {
+		if (BT_TEST(drp->dr_bitmap, reg) == 0) {
+			BT_SET(drp->dr_bitmap, reg);
+			return (reg);
 		}
 	}
-
 	return (-1); /* no available registers */
 }
 
