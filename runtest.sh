3cf4fabd263bf9a98c596a1acdd5235bc07b1c93
Eugene Loh <eugene.loh@oracle.com>
2020-07-30 13:34:32 -0400

    Add pre-release banner UNLESS in test suite
    A previous commit added a pre-release banner.  The commit message
    said testing was decommissioned, but that was not the case; there
    were simply widespread failures.  A later commit instituted some
    mechanisms to filter out the banner, but they did not cover every
    case, resulting in at least one known test FAIL.
    
    Suppress the banner when the test suite is being run.  Remove the
    filtering mechanisms.
    
    Orabug: 31694125
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>

--- runtest.sh
+++ runtest.sh
@@ -465,8 +465,7 @@ postprocess()
     local final=$3
     local retval=0
 
-    grep -v "^DTrace [^\[]* \[Pre-Release with limited functionality\]$" $output > $tmpdir/pp.out
-    cp -f $tmpdir/pp.out $output
+    cp -f $output $tmpdir/pp.out
 
     # Postprocess the output, if need be.
     if [[ -x $postprocessor ]]; then
@@ -584,9 +583,7 @@ for dt in $dtrace; do
     fi
 
     # Write out a list of loaded providers.
-    DTRACE_DEBUG= $dt -l | \
-	grep -v "^DTrace [^\[]* \[Pre-Release with limited functionality\]$" | \
-	tail -n +2 | awk '{print $2;}' | sort -u > $tmpdir/providers
+    DTRACE_DEBUG= $dt -l | tail -n +2 | awk '{print $2;}' | sort -u > $tmpdir/providers
 
     unset LD_LIBRARY_PATH
     break
