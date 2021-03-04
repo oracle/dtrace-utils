737c78144475651d02e81b147d10cf8eb3410973
Eugene Loh <eugene.loh@oracle.com>
2021-03-03 19:59:10 -0500

    Clean up the bpflogsize tests
    The bpflogsize tests are tricky, because:
    - they rely on D scripts that fail the BPF verifier
    - their BPF logs are too big for the default BPF log size
    - their BPF logs will fit if bpflogsize is made large enough
    
    The first question is what D script will fail the BPF verifier?  Who is
    emitting bad BPF code?  The GCC cross compiler or our code generation?
    
    The trick is to emit legimate BPF code of such complexity that the BPF
    verifier cannot handle it.  The D quantize() aggregation function is a
    good candidate in this regard, since it quantizes values to one of 127
    different bins in highly branched code.  Repeated calls to quantize()
    overwhelm the BPF verifier.
    
    How many calls are needed depends on the underlying implementation, and
    when DT_AGG_NUM_COPIES=1 was introduced the bpflogsize tests changed
    behavior.
    
    Once the number of quantize() calls is increased sufficiently, the
    emitted code fails the BPF verifier, albeit with long verification
    times and huge BPF log files.
    
    It turns out that by increasing the number of quantize() calls even
    further, the verifier walks branches differently and becomes overwhelmed
    earlier, requiring 10x shorter verification time or log files.
    
    Therefore, overhaul the tests:
    - to make them more clear
    - to induce the quicker path to BPF verification failure
    - to eliminate the extended time outs
    
    In the future, changes in the quantize() implementation or in the BPF
    verifier may require further changes in these tests.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/misc/tst.bpflogsize-cmdline.sh
+++ test/unittest/misc/tst.bpflogsize-cmdline.sh
@@ -5,7 +5,6 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
-# @@timeout: 120
 
 dtrace=$1
 
@@ -13,17 +12,18 @@ DIRNAME="$tmpdir/misc-BPFlog-cmdline.$$.$RANDOM"
 mkdir -p $DIRNAME
 cd $DIRNAME
 
-# form a D script that will not run with defaults and will require a large BPF log file
+# form a D script that:
+# - fails the BPF verifier (due to its high complexity)
+# - requires a larger-than-default-size BPF log file
 
 cat > D.d << EOF
-#pragma D option quiet
 BEGIN
 {
 	x = 1;
 EOF
 
-for x in `seq 100`; do
-	echo "	@ = quantize(x); @ = quantize(x); @ = quantize(x); @ = quantize(x);" >> D.d
+for x in `seq 2000`; do
+	echo "	@ = quantize(x);" >> D.d
 done
 
 cat >> D.d << EOF
@@ -32,77 +32,94 @@ cat >> D.d << EOF
 EOF
 
 # form the expected output when the BPF log size is too small
-# ("nn" represents the size that appears in the actual output)
+# ("nnnn" represents the size that appears in the actual output)
 
 cat > D.out << EOF
 BPF verifier log is incomplete and is not reported.
 Set DTrace option 'bpflogsize' to some greater size for more output.
-(Current size is nn.)
-dtrace: could not enable tracing: BPF program load for 'dtrace:::BEGIN' failed: Argument list too long
+(Current size is nnnn.)
+dtrace: could not enable tracing: BPF program load for 'dtrace:::BEGIN' failed: Bad address
 EOF
+
+# dump files
+function dump_files() {
+	# the output (just the head and tail if it is huge)
+	echo "========== output"
+	if [ `cat tmp.out | wc -l` -gt 30 ]; then
+		head -10 tmp.out
+		echo "..."
+		tail -10 tmp.out
+	else
+		cat tmp.out
+	fi
+
+	# the D script (which has a large number of duplicate lines)
+	echo "========== uniq -d D.d"
+	uniq -c D.d
+	echo "=========="
+}
 
 # try the script with increasing BPF log size (starting with default)
 
-startedtoosmall=0
-cursiz=$((16 * 1024 * 1024 - 1))
-option=""
-while [ $cursiz -lt $((2 * 1024 * 1024 * 1024)) ]; do
+cursiz=$((16 * 1024 * 1024 - 1))     # default size from dt_bpf_load_prog()
+option=""                            # option is blank for default
+while [ $cursiz -lt $((1024 * 1024 * 1024)) ]; do
 
 	# dtrace should not pass
-	$dtrace $option -s D.d >& tmp.out
+	$dtrace $option -qs D.d >& tmp.out
 	if [ $? -eq 0 ]; then
 		echo unexpected pass
-		cat D.d
-		cat tmp.out
+		dump_files
 		exit 1
 	fi
 
-	# usually, it will fail because the BPF log size is too small
-	if [ `sed s/$cursiz/nn/ tmp.out | diff - D.out | wc -l` -eq 0 ]; then
+	# Usually (at least in the default case),
+	# dtrace should fail because the BPF log size is too small.
+	# Output should match D.out.
+	if [ `sed s/$cursiz/nnnn/ tmp.out | diff - D.out | wc -l` -eq 0 ]; then
 		echo okay: $cursiz is too small
 
-		# confirm that we started too small
-		startedtoosmall=1
-
-		# so bump the size up and try again
+		# bump the size up and try again
 		cursiz=$((2 * $cursiz + 1))
 		option="-xbpflogsize=$cursiz"
 		continue
-	fi
-
-	# confirm that we started too small
-	if [ $startedtoosmall -eq 0 ]; then
-		echo the smallest sizes we try should be too small
+	elif [ -z $option ]; then
+		echo "The default case is expected to have bpflogsize too small"
+		echo "and fail with the expected error."
+		echo "========== output (expected)"
+		sed 's/nnnn/'$cursiz'/' D.out
+		dump_files
 		exit 1
 	fi
 
-	# otherwise, it should fail with a huge dump
-	actsiz=`cat tmp.out | wc -c`
+	# determine the actual size of the BPF log
+	# (It's the "BPF:" tagged output, minus the "BPF:" prefix on each line.)
+	actsiz=`grep "^BPF: " tmp.out | cut -c6- | wc -c`
+
+	# confirm that we failed with a huge dump
 	status=0
 	if [ $actsiz -lt $(($cursiz / 2)) ]; then
 		echo ERROR: BPF output should have fit in smaller buffer
 		status=1
 	fi
 	if [ $actsiz -gt $cursiz ]; then
-		echo ERROR: BPF output is larger than user-specified limit
+		echo ERROR: BPF output is larger than user-specified limit: $actsiz '>' $cursiz
 		status=1
 	fi
 	if [ `grep -cv '^BPF: ' tmp.out` -ne 1 ]; then
 		echo ERROR: expected only one non-\"BPF:\" output line
 		status=1
 	fi
-	if ! grep -q '^BPF: BPF program is too large. Processed .* insn' tmp.out; then
+	if ! grep -q '^BPF: The sequence of 8193 jumps is too complex.' tmp.out; then
 		echo ERROR: BPF error message is missing
 		status=1
 	fi
-	if ! grep -q '^dtrace: could not enable tracing: BPF program load for .* failed: Argument list too long' tmp.out; then
+	if ! grep -q '^dtrace: could not enable tracing: BPF program load for '\''dtrace:::BEGIN'\'' failed: Bad address' tmp.out; then
 		echo ERROR: dtrace error message is missing
 		status=1
 	fi
 	if [ $status -ne 0 ]; then
-		head -10 tmp.out
-		echo "..."
-		tail -10 tmp.out
+		dump_files
 	else
 		echo SUCCESS: test failed with expected error
 	fi
