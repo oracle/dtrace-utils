f35847f94a0926674dc2203d017787f803b02190
Kris Van Hees <kris.van.hees@oracle.com>
2021-06-08 10:27:06 -0400

    Mark dynamic data trace() test as PASS
    This test was failing because of a wrong line number in the expected
    output.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Eugene Loh <eugene.loh@oracle.com>

--- test/unittest/actions/trace/err.D_TRACE_DYN.d
+++ test/unittest/actions/trace/err.D_TRACE_DYN.d
@@ -1,10 +1,10 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
-/* @@xfail: dtv2 */
+
 /*
  * ASSERTION: The trace() action does not accept a dynamic expression as value.
  *
