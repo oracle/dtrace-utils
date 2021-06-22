b61627e0fe222c4dc3190c99b0ec8bcd57dae1a8
Kris Van Hees <kris.van.hees@oracle.com>
2021-06-21 22:56:59 -0400

    The fbtprovider/tst.return0.d test should not use ioctl
    This test was triggering on ioctl because that was the system call used
    by DTrace to read trace data.  It was therefore guaranteed to be called
    frequently.
    
    With the new implementation ioctl is no longer used.  Instead, we can
    trigger on the poll system call.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Eugene Loh <eugene.loh@oracle.com>

--- test/unittest/fbtprovider/tst.return0.d
+++ test/unittest/fbtprovider/tst.return0.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2021, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -11,14 +11,12 @@
  * SECTION: FBT Provider/Probe arguments
  */
 
-/* @@xfail: dtv2 */
 /* @@runtest-opts: -Z */
 
 #pragma D option quiet
 #pragma D option statusrate=10ms
 
-fbt::SyS_ioctl:return,
-fbt::__x64_sys_ioctl:return
+fbt::do_sys_poll:return
 /arg1 == 0/
 {
 	printf("%s %x returned 0", probefunc, arg0);
