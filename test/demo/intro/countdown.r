153364bf52d44b4e10849892e37826f617fa38a6
Nick Alcock <nick.alcock@oracle.com>
2021-09-07 13:52:46 +0100

    tests: fix two more trivially passing tests
    These tests just need results regenerating to account for tiny spacing
    changes.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>

--- test/demo/intro/countdown.r
+++ test/demo/intro/countdown.r
@@ -1,14 +1,14 @@
                    FUNCTION:NAME
-                      :tick-1sec        10
-                      :tick-1sec         9
-                      :tick-1sec         8
-                      :tick-1sec         7
-                      :tick-1sec         6
-                      :tick-1sec         5
-                      :tick-1sec         4
-                      :tick-1sec         3
-                      :tick-1sec         2
-                      :tick-1sec         1
+                      :tick-1sec          10
+                      :tick-1sec           9
+                      :tick-1sec           8
+                      :tick-1sec           7
+                      :tick-1sec           6
+                      :tick-1sec           5
+                      :tick-1sec           4
+                      :tick-1sec           3
+                      :tick-1sec           2
+                      :tick-1sec           1
                       :tick-1sec   blastoff!                        
 
 -- @@stderr --
