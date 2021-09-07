85ecfd9d68bba44b56a606e79f8ef5a43ecb7047
Nick Alcock <nick.alcock@oracle.com>
2021-09-07 13:44:48 +0100

    tests: printa/tst.many.d is not relevant for DTrace based on BPF
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/printa/tst.many.d
+++ test/unittest/printa/tst.many.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2021, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -9,7 +9,7 @@
  * Don't time out until the buffer fills.  That might take some time, so
  * set the timeout to a high value to, in effect, disable it.
  */
-/* @@xfail: dtv2 */
+/* @@skip: dtv2 */
 /* @@timeout: 600 */
 
 #pragma D option bufsize=32m
