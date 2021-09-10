8a7347fb4a5273e3a4d8981049824529c2e7f915
Kris Van Hees <kris.van.hees@oracle.com>
2021-09-10 01:03:03 -0400

    Fix up test cases affected by libctf vs libdtrace-ctf incompatibilities
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/offsetof/err.D_UNKNOWN.OffsetofNULL.r
+++ test/unittest/offsetof/err.D_UNKNOWN.OffsetofNULL.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/offsetof/err.D_UNKNOWN.OffsetofNULL.d: [D_UNKNOWN] line 26: failed to determine offset of NULL: Member name not found
+dtrace: failed to compile script test/unittest/offsetof/err.D_UNKNOWN.OffsetofNULL.d: [D_UNKNOWN] line 26: failed to determine offset of NULL: Member name not found.
--- test/unittest/types/err.D_UNKNOWN.dupenum.r
+++ test/unittest/types/err.D_UNKNOWN.dupenum.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/types/err.D_UNKNOWN.dupenum.d: [D_UNKNOWN] line 18: failed to define enumerator 'x': Duplicate member or variable name
+dtrace: failed to compile script test/unittest/types/err.D_UNKNOWN.dupenum.d: [D_UNKNOWN] line 18: failed to define enumerator 'x': Duplicate member or variable name.
