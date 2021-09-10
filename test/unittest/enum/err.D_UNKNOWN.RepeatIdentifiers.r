6ecea92144c8cecfb5c0a5f5f6cb40285f15e82e
Kris Van Hees <kris.van.hees@oracle.com>
2021-09-10 02:00:03 -0400

    Fix another test in view of libctf differences
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/enum/err.D_UNKNOWN.RepeatIdentifiers.r
+++ test/unittest/enum/err.D_UNKNOWN.RepeatIdentifiers.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/enum/err.D_UNKNOWN.RepeatIdentifiers.d: [D_UNKNOWN] line 24: failed to define enumerator 'GREEN': Duplicate member or variable name
+dtrace: failed to compile script test/unittest/enum/err.D_UNKNOWN.RepeatIdentifiers.d: [D_UNKNOWN] line 24: failed to define enumerator 'GREEN': Duplicate member or variable name.
