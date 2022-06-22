69a55492a9461f9bf721fb36d6ca37fe8a723fe9
Eugene Loh <eugene.loh@oracle.com>
2022-06-22 19:02:57 -0400

    test: Add XFAIL placeholder: DTrace internal integer representation
    When DTrace performs integer operations, it assumes integers (of
    all widths and signedness) are internally represented in 64 bits
    and do not need frequent typecasting.  This breaks the documented
    behavior.  Recent patches have addressed a number of these problems.
    
    A few problems remain.  Notably, arithmetic operations can change
    the sign bit of a narrow integer.  This should result in the high
    bits of the 64-bit representation changing.  The DTrace code generator
    should typecast appropriately.
    
    Add a placeholder test that XFAILs to indicate that work remains.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/arithmetic/tst.bug-casting.r
@@ -0,0 +1,13 @@
+ffffffff80000000
+ffffffff80000000
+100000000
+100000000
+ffffffff80000000
+ffffffff80000000
+0
+0
+flip sign bit of +1, result is negative
+flip sign bit of +1, result is negative
+flip sign bit of -1, result is positive
+flip sign bit of -1, result is positive
+
