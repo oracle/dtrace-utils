8fcffe86552e85f7c9aef438e070fd31c2047a86
eugene.loh@oracle.com <eugene.loh@oracle.com>
2022-07-27 00:56:48 -0400

    test: Clean up bad-cast test
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/types/err.D_CAST_INVAL.badcast.d
+++ test/unittest/types/err.D_CAST_INVAL.badcast.d
@@ -1,23 +1,16 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 
-
 /*
- * ASSERTION:
- *	Test an illegal cast - make sure dtrace catches it.
- *
- * SECTION: Aggregations/Clearing aggregations
- *
- *
+ * ASSERTION: Test an illegal cast - make sure dtrace catches it.
  */
-
 
 BEGIN
 {
 	(struct task_struct)trace(`max_pfn);
-	exit();
+	exit(0);
 }
