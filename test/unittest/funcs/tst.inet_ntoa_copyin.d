5d0d980181d179f444445792dc13b2cfcf0015d7
Eugene Loh <eugene.loh@oracle.com>
2022-11-03 15:52:19 -0400

    Support inet_ntoa() for addresses that are not DTrace pointers
    If the input argument to inet_ntoa() is not a DTrace pointer, the
    BPF verifier cannot confirm it is safe.  So first copy the input
    data to the BPF stack using the probe_read() helper function.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/funcs/tst.inet_ntoa_copyin.d
@@ -0,0 +1,23 @@
+/*
+ * Oracle Linux DTrace.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+#pragma D option quiet
+#pragma D option destructive
+
+BEGIN
+{
+	system("printf '\xc0\xa8\x01\x17' > /dev/null 2>&1");
+}
+
+syscall::write:entry
+/ppid == $pid/
+{
+	printf("%s\n", inet_ntoa(copyin(arg1,4)));
+	exit(0);
+}
+
+ERROR { exit(1); }
