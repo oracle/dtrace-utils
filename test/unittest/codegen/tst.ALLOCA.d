a7ee5e88f06d71cb65febe5e33299a06d35eb866
Eugene Loh <eugene.loh@oracle.com>
2022-11-03 15:52:14 -0400

    test: Add test to check ALLOCA flag propagation
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/codegen/tst.ALLOCA.d
@@ -0,0 +1,24 @@
+/*
+ * Oracle Linux DTrace.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/*
+ * Check the ALLOCA annotations in the parse tree.
+ */
+
+/* @@runtest-opts: -xtree=4 -e */
+/* @@nosort */
+
+syscall::write:entry
+{
+  myvar_int = 4;
+  myvar_string = "abcde";
+  myvar_alloca = alloca(4);
+  myvar_basename = basename("/foo/bar");
+  myvar_copyin = copyin(arg1, 4);
+  myvar_copyinstr = copyinstr(arg1);
+  myvar_dirname = dirname("/foo/bar");
+}
