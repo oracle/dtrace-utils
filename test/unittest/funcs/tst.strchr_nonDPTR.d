0903985768bc950c5e36f96d36ac3311542c7c5c
Eugene Loh <eugene.loh@oracle.com>
2022-11-03 15:52:18 -0400

    Support strrchr() for addresses that are not DTrace pointers
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/funcs/tst.strchr_nonDPTR.d
@@ -0,0 +1,25 @@
+/*
+ * Oracle Linux DTrace.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+#pragma D option quiet
+#pragma D option destructive
+
+BEGIN
+{
+	/* "abcdef" */
+	system("printf '\x61\x62\x63\x64\x65\x66' > /dev/null 2>&1");
+}
+
+syscall::write:entry
+/ppid == $pid/
+{
+	printf("|%s|\n", strchr((void *)arg1, 'b'));
+	printf("|%s|\n", strrchr((void *)arg1, 'b'));
+	exit(0);
+}
+
+ERROR { exit(1); }
---
+++ test/unittest/funcs/tst.strchr_nonDPTR.r
@@ -0,0 +1,3 @@
+|bcdef|
+|bcdef|
+
