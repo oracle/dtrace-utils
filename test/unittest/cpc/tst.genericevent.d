4734ef302cf78b41a9481259d1f2d9da88a08cef
Eugene Loh <eugene.loh@oracle.com>
2023-01-10 13:31:07 -0500

    cpc: Update the Solaris generic-event test
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/cpc/tst.genericevent.d
+++ test/unittest/cpc/tst.genericevent.d
@@ -1,30 +1,28 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 
 /*
  * Test that we can successfully enable a probe using a generic event.
- * Currently, all platforms implement 'PAPI_tot_ins' so we'll use that.
+ * Currently, all platforms implement 'cpu_clock' so we'll use that.
  * Note that this test will fail if the system under test does not
  * implement that event.
  *
  * This test will fail if:
- *	1) The system under test does not define the 'PAPI_tot_ins' event.
+ *	1) The system under test does not define the 'cpu_clock' event.
  */
 
 #pragma D option quiet
-#pragma D option bufsize=128k
 
-cpc:::PAPI_tot_ins-all-10000
+cpc:::cpu_clock-all-10000
 {
 	@[probename] = count();
 }
 
-tick-1s
-/n++ > 10/
+tick-3s
 {
 	exit(0);
 }
