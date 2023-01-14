16c18d1c76e1a1be12d650b9754d22286f42fcc3
Eugene Loh <eugene.loh@oracle.com>
2023-01-14 18:19:53 -0500

    test: Fix a tiny typo
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/codegen/tst.tstring_asgn_expr.d
+++ test/unittest/codegen/tst.tstring_asgn_expr.d
@@ -1,12 +1,12 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 
 /*
- * Ensure tstrings are handled correctly for non-void assignment expressoions.
+ * Ensure tstrings are handled correctly for non-void assignment expressions.
  */
 
 #pragma D option quiet
