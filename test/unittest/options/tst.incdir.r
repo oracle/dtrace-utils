3bc04c20c2543e97a970d9da587e71a47b85535b
Eugene Loh <eugene.loh@oracle.com>
2023-01-19 15:41:00 -0500

    test: Add test for incdir option
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/options/tst.incdir.r
@@ -0,0 +1,8 @@
+script fails by default as expected
+good
+
+script runs with incdir
+-- @@stderr --
+/dev/stdin:1: fatal error: D.h: No such file or directory
+compilation terminated.
+dtrace: failed to compile script D.d: Preprocessor failed to process input program
