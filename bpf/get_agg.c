f76ebb2206888cd6ff9551323c7900a6d81f6d56
Kris Van Hees <kris.van.hees@oracle.com>
2023-02-01 01:54:59 -0500

    bpf: add agggen handling to dt_get_agg()
    The dt_get_agg() function will now consult the generation value for the
    aggregation id.  If existing data is of an earlier generation, the data
    is reset.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Eugene Loh <eugene.loh@oracle.com>

--- bpf/get_agg.c
+++ bpf/get_agg.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  */
 #include <linux/bpf.h>
 #include <stdint.h>
@@ -11,17 +11,25 @@
 # define noinline	__attribute__((noinline))
 #endif
 
+extern struct bpf_map_def agggen;
+
 /*
  * Get a pointer to the data storage for an aggregation.  Regular aggregations
  * are stored as an indexed aggregation with an empty key.  The 'id' parameter
- * is the aggregtion id; 'ival' is the initial value for min/max aggregations
+ * is the aggregation id; 'ival' is the initial value for min/max aggregations
  * (and otherwise 0).  The 'dflt' parameter is a pointer to a zero-filled area
  * of memory that is at least the size of the largest aggregation.
  */
 noinline uint64_t *dt_get_agg(const dt_dctx_t *dctx, uint32_t id,
 			      const char *key, uint64_t ival, const char *dflt)
 {
+	uint64_t	*genp;
 	uint64_t	*valp;
+
+	/* get the gen value */
+	genp = bpf_map_lookup_elem(&agggen, &id);
+	if (genp == 0)
+		return 0;
 
 	/* place the variable ID at the beginning of the key */
 	*(uint32_t *)key = id;
@@ -29,8 +37,8 @@ noinline uint64_t *dt_get_agg(const dt_dctx_t *dctx, uint32_t id,
 	/* try to look up the key */
 	valp = bpf_map_lookup_elem(dctx->agg, key);
 
-	/* if not found, create it */
-	if (valp == 0) {
+	/* if not found, or older gen, set initial values */
+	if (valp == 0 || valp[0] < *genp) {
 		/* start with all zeroes */
 		if (bpf_map_update_elem(dctx->agg, key, dflt, BPF_ANY) < 0)
 			return 0;
@@ -42,10 +50,10 @@ noinline uint64_t *dt_get_agg(const dt_dctx_t *dctx, uint32_t id,
 		/* ival is nonzero only for min() and max() */
 		if (ival)
 			valp[1] = ival;
-	}
 
-	/* increment the data counter */
-	valp[0] += 1;
+		/* set the gen value */
+		valp[0] = *genp;
+	}
 
 	/* advance past the data counter */
 	valp += 1;
