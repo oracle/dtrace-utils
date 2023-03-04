82a067cb21dc1b2753029e2bf6e3031632c6c2a5
Eugene Loh <eugene.loh@oracle.com>
2023-03-04 17:36:38 -0500

    test: Fix scripting test for "quiet"
    This test was broken because the runtest.sh test harness was ignoring
    the initial #! line, invoking the test as a D script rather than as an
    interpreter file.  Hence, the -q was being ignored;  the .r results
    file fit the actual (incorrect) behavior rather than the anticipated
    (correct) behavior.
    
    Use interpreter-file support in runtest.sh to launch the test as an
    interpreter file.  Fix the results file to match expected "quiet"
    results.  Rename the file from "quite" to "quiet."
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/scripting/tst.quiet.d
+++ test/unittest/scripting/tst.quiet.d
@@ -1,8 +1,8 @@
-#!/usr/sbin/dtrace -qs
+#!dtrace -qs
 
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
--- test/unittest/scripting/tst.quite.r
+++
@@ -1,5 +0,0 @@
-                   FUNCTION:NAME
-                          :BEGIN 
-
--- @@stderr --
-dtrace: script 'test/unittest/scripting/tst.quite.d' matched 1 probe
