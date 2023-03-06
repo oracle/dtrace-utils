d51a52b13d7116da8b7024bb9885842e0b4e3e8f
Eugene Loh <eugene.loh@oracle.com>
2023-03-06 16:11:26 -0500

    test: Switch kernel pointer in tracemem() test
    The tracemem() test dumped a portion of kernel memory, comparing
    different tracemem() output for the same kernel memory for consistency.
    Since it was looking at task_struct for the current thread, however,
    the underlying data could conceivably change as the test ran, leading
    to inconsistency and a false negative.
    
    Change the kernel memory to be the first 14 bytes of `linux_banner,
    which should remain "Linux version ".
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/tracemem/tst.tracemem.r
+++ test/unittest/tracemem/tst.tracemem.r
@@ -1,9 +1,22 @@
-256 bytes
-256 bytes
-32 bytes
-64 bytes
-128 bytes
-256 bytes
-256 bytes
-128 bytes
-256 bytes
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76 65 72 73 69 6f 6e 20        Linux version 
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76 65 72 73 69 6f 6e 20        Linux version 
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69                                            Li
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76                             Linux v
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76 65 72 73 69 6f              Linux versio
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76 65 72 73 69 6f 6e 20        Linux version 
+
+             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  0123456789abcdef
+         0: 4c 69 6e 75 78 20 76 65 72 73 69 6f 6e 20        Linux version 
+
