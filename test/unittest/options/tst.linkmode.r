dc7e6955a88f4bd8d06b91f86cf4b06fc6a3d488
Eugene Loh <eugene.loh@oracle.com>
2023-03-11 12:30:22 -0500

    test: Add test for option "linkmode"
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/options/tst.linkmode.r
@@ -0,0 +1,17 @@
+try
+Linux version 
+
+try -xlinkmode=static
+Linux version 
+
+try -xlinkmode=kernel
+Linux version 
+
+try -xlinkmode=foo
+try -xlinkmode=dynamic
+try -xlinkmode=dynamic -xknodefs
+Linux version 
+
+-- @@stderr --
+dtrace: failed to set -x linkmode: Invalid value for specified option
+dtrace: invalid probe specifier BEGIN { trace((string)`linux_banner); exit(0); }: relocation remains against kernel symbol vmlinux`linux_banner (offset {ptr})
