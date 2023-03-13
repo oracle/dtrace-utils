50f66f4310334a97d088148bcbd9b23694cd4119
Eugene Loh <eugene.loh@oracle.com>
2023-03-13 19:14:08 -0400

    test: Add test for option "switchrate"
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/options/tst.switchrate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Oracle Linux DTrace.
+# Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# http://oss.oracle.com/licenses/upl.
+#
+
+# @@nosort
+
+dtrace=$1
+
+# Currently, there are few easily visible effects of changing the switchrate.
+# One way to check is to run a very simple D script that should run
+# "instantaneously" -- except that its termination will be delayed by a
+# very long switchrate.
+
+status=0
+for nexpect in 1 10; do
+	# Run the "instantaneous" D script with the prescribed switchrate.
+	# Time it.  Round to the nearest number of seconds with int(t+0.5).
+	# Go ahead and lop off a second just because we expect the script
+	# to take *some* time.
+	nactual=`/usr/bin/time -f "%e" \
+	    $dtrace -xswitchrate=${nexpect}sec -qn 'BEGIN { exit(0) }' \
+	    |& awk 'NF != 0 {print int($1 + 0.5) - 1}'`
+
+	# Check the actual number of seconds to the expected value.
+	test/utils/check_result.sh $nactual $nexpect 1
+	status=$(($status + $?))
+done
+
+exit $status
