525b8c4764ec9ac9a995398c9529e4d664995364
Kris Van Hees <kris.van.hees@oracle.com>
2023-03-30 02:02:35 -0400

    fbt: ignore compiler-generated internal symbols
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Eugene Loh <eugene.loh@oracle.com>

--- libdtrace/dt_prov_fbt.c
+++ libdtrace/dt_prov_fbt.c
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  *
@@ -99,6 +99,10 @@ static int populate(dtrace_hdl_t *dtp)
 			if (*p == '[')
 				p++;
 		}
+
+		/* Weed out synthetic symbol names (that are invalid). */
+		if (strchr(buf, '.') != NULL)
+			continue;
 
 		/*
 		 * If we did not see a module name, perform a symbol lookup to
