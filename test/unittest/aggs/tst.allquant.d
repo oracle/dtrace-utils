5ad4703d88db08b741b994ae1c960a907ca7e465
Eugene Loh <eugene.loh@oracle.com>
2023-05-30 18:15:14 -0400

    test: Convert tick-* probes to ioctl:entry for tst.allquant.d
    Historically, many tests have used tick-* probes to get multiple
    probe firings, but those probes can be unreliable, depending on
    how a kernel is configured.  Tests that required very many probe
    firings have been converted to ioctl:entry.  Tests that required
    very few have been left alone.
    
    Convert this "in-between" test as well.  It normally passes, but
    with variable execution time and has even been observed to time out.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/aggs/tst.allquant.d
+++ test/unittest/aggs/tst.allquant.d
@@ -1,15 +1,18 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
+/* @@trigger: bogus-ioctl */
+/* @@nosort */
 
 #pragma D option quiet
 
 int i;
 
-tick-10ms
+syscall::ioctl:entry
+/pid == $target/
 {
 	@ = quantize(1LL << i);
 	@ = quantize((1LL << i) + 1);
@@ -18,8 +21,8 @@ tick-10ms
 	i++;
 }
 
-tick-10ms
-/i == 64/
+syscall::ioctl:entry
+/pid == $target && i == 64/
 {
 	exit(0);
 }
