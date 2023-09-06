f9f33caaec325e346a1d470adf442b813b3d41b3
Eugene Loh <eugene.loh@oracle.com>
2023-09-06 13:01:33 -0400

    test: Convert tick-* probes to ioctl:entry for tst.quantmany.d
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/aggs/tst.quantmany.d
+++ test/unittest/aggs/tst.quantmany.d
@@ -1,34 +1,37 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
+/* @@trigger: bogus-ioctl */
+/* @@nosort */
 
 #pragma D option quiet
 
 int64_t val, shift;
 
-tick-1ms
-/val == 0/
+syscall::ioctl:entry
+/pid == $target && val == 0/
 {
 	val = -(1 << shift);
 	shift++;
 }
 
-tick-1ms
-/shift == 32/
+syscall::ioctl:entry
+/pid == $target && shift == 32/
 {
 	exit(0);
 }
 
-tick-1ms
-/val == -1/
+syscall::ioctl:entry
+/pid == $target && val == -1/
 {
 	val = (1 << shift);
 }
 
-tick-1ms
+syscall::ioctl:entry
+/pid == $target/
 {
 	@[shift] = quantize(val, val);
 	val >>= 1;
