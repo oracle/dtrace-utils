6029a38d266acab431382790d0248b453937e7a1
Eugene Loh <eugene.loh@oracle.com>
2023-09-06 13:14:51 -0400

    test: Convert tick-* probes to ioctl:entry for tst.negquant.d
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/aggs/tst.negtrunc.d
+++ test/unittest/aggs/tst.negtrunc.d
@@ -1,23 +1,25 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2023, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
+/* @@trigger: bogus-ioctl */
+/* @@nosort */
 
 #pragma D option quiet
 
 int i;
 
-tick-10ms
-/i < 100/
+syscall::ioctl:entry
+/pid == $target && i < 100/
 {
 	@[i] = sum(i);
 	i++;
 }
 
-tick-10ms
-/i == 100/
+syscall::ioctl:entry
+/pid == $target && i == 100/
 {
 	exit(0);
 }
