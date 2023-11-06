68a085db051067b1875a102f4d1ff887047742e9
Eugene Loh <eugene.loh@oracle.com>
2023-11-06 15:10:28 -0500

    Fix sizw typo
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- libdtrace/dt_impl.h
+++ libdtrace/dt_impl.h
@@ -330,7 +330,7 @@ struct dtrace_hdl {
 	uint_t dt_maxdvarsize;	/* largest dynamic variable across programs */
 	uint_t dt_maxtuplesize;	/* largest tuple across programs */
 	uint_t dt_maxlvaralloc;	/* largest lvar alloc across pcbs */
-	uint_t dt_maxaggdsize;	/* largest aggregation data sizw */
+	uint_t dt_maxaggdsize;	/* largest aggregation data size */
 	dt_tstring_t *dt_tstrings; /* temporary string slots */
 	dt_list_t dt_modlist;	/* linked list of dt_module_t's */
 	dt_htab_t *dt_mods;	/* hash table of dt_module_t's */
