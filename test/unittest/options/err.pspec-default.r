95140dae9d30e22558e94263db13338303739932
Nick Alcock <nick.alcock@oracle.com>
2023-12-11 20:37:47 +0000

    Revert "test, options: work around a subtle lexer bug"
    This reverts commit ae9f2a137438b2e3798ff532bd986fe3abfd9c8a.
    
    (Technically this is not a strict reversion: we fix the line number too,
    because the previous fixes slightly change the reported error.  But
    conceptually it is one.)
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/options/err.pspec-default.r
+++ test/unittest/options/err.pspec-default.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/options/err.pspec-default.d: line 14: syntax error near
+dtrace: failed to compile script test/unittest/options/err.pspec-default.d: line 15: syntax error near end-of-input
