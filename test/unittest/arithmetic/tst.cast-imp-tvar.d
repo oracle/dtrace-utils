8cf714dbd0ba5f0bed0da350a600ebc857670a75
eugene.loh@oracle.com <eugene.loh@oracle.com>
2024-03-11 21:00:52 -0400

    test: Remove unnecessary preprocessor macro
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/arithmetic/tst.cast-imp-tvar.d
+++ test/unittest/arithmetic/tst.cast-imp-tvar.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2024, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -11,7 +11,7 @@
  *
  * SECTION: Types, Operators, and Expressions/Arithmetic Operators
  */
-/* @@runtest-opts: -qC -DUSE_AS_D_SCRIPT */
+/* @@runtest-opts: -qC */
 
 self signed char c, c0;
 self short s, s0;
