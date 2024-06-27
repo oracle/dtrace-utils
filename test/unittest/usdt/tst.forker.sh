5a9d05c14f911de0fe3eafe889f3218349b424a0
Eugene Loh <eugene.loh@oracle.com>
2024-06-27 01:25:16 -0400

    test: Check dtrace return status in USDT tst.forker.sh
    This test was spuriously passing.  DTrace failed to run -- many error
    messages in the log file indicated as much -- and yet the test passed.
    
    Check the dtrace return status.
    
    The test will pass once USDT supports wildcard provider names.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/usdt/tst.forker.sh
+++ test/unittest/usdt/tst.forker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 # Oracle Linux DTrace.
-# Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
 #
 # @@timeout: 120
+# @@xfail: dtv2 USDT wildcard
 
 if [ $# != 1 ]; then
 	echo expected one argument: '<'dtrace-path'>'
@@ -32,6 +33,10 @@ while kill -0 $id >/dev/null 2>&1; do
 			exit(0);
 		}
 	EOF
+	if [ $? -ne 0 ]; then
+		echo ERROR: DTrace failed to run
+		exit 1
+	fi
 done
 
 exit 0
