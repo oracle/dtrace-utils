22b24012109bf79522bc0f2e2d2a9c5b0136135f
Kris Van Hees <kris.van.hees@oracle.com>
2025-04-30 20:43:48 -0400

    usdt: add HASUSDT to the helper ioctl interface
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Eugene Loh <eugene.loh@oracle.com>

--- include/dtrace/ioctl.h
+++ include/dtrace/ioctl.h
@@ -2,7 +2,7 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  *
- * Copyright (c) 2009, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2025, Oracle and/or its affiliates. All rights reserved.
  */
 
 #ifndef _DTRACE_IOCTL_H_
@@ -15,5 +15,6 @@
 #define DTRACEHIOC_ADD		_IOW(DTRACEHIOC, 1, dof_hdr_t)
 #define DTRACEHIOC_REMOVE	_IOW(DTRACEHIOC, 2, int)
 #define DTRACEHIOC_ADDDOF	_IOW(DTRACEHIOC, 3, dof_helper_t)
+#define DTRACEHIOC_HASUSDT	_IOW(DTRACEHIOC, 4, void *)
 
 #endif /* _DTRACE_IOCTL_H */
