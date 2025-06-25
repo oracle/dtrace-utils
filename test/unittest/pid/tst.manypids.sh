c42d8c095086adb2597f869d6a8143b68d01fe27
Eugene Loh <eugene.loh@oracle.com>
2025-06-25 02:03:05 -0400

    test: Extend timeout for many-pids test
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>

--- test/unittest/pid/tst.manypids.sh
+++ test/unittest/pid/tst.manypids.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 # Oracle Linux DTrace.
-# Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
+# @@timeout: 80
 
 if [ $# != 1 ]; then
 	echo expected one argument: '<'dtrace-path'>'
