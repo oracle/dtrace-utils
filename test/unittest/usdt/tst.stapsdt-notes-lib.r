5771baf4b55eb703e7be05306a6bbd87aa2350f3
Alan Maguire <alan.maguire@oracle.com>
2025-07-30 10:01:45 +0100

    selftests/usdt: add test for stapsdt notes in shared library
    To ensure stapsdt notes are found/fire for shared libraries, create
    a shared library and trace the stapsdt probes in it.  To ensure the
    library is loaded when DTrace has started, call it early in the
    program lifetime and sleep until DTrace starts; at that point
    trace the probes with the pid and ensure they fire with expected args.
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ test/unittest/usdt/tst.stapsdt-notes-lib.r
@@ -0,0 +1,14 @@
+libstapsdttest.so:libfn:zero
+libstapsdttest.so:libfn:one:1
+libstapsdttest.so:libfn:two:2:3
+libstapsdttest.so:libfn:three:4:5:7
+libstapsdttest.so:libfn:four:7:8:9:10
+libstapsdttest.so:libfn:five:11:12:13:14:15
+libstapsdttest.so:libfn:six:16:17:18:19:20:21
+libstapsdttest.so:libfn:seven:22:23:24:25:26:27:28
+libstapsdttest.so:libfn:eight:29:30:31:32:33:34:35:36
+libstapsdttest.so:libfn:nine:37:38:39:40:41:42:43:44:45
+libstapsdttest.so:libfn:ten:46:47:48:49:50:51:52:53:54:55
+libstapsdttest.so:libfn:eleven:56:57:58:59:60:61:62:63:64:65
+libstapsdttest.so:libfn:twelve:67:68:69:70:71:72:73:74:75:76
+
