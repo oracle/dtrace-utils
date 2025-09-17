edfbf0ddd973e39afb7006e9c3f77e97eada68a4
Elena Zannoni <elena.zannoni@oracle.com>
2025-09-17 14:54:25 -0600

    Add license and copyright information to the examples

--- examples/language_features/300actions-exit.sh
+++ examples/language_features/300actions-exit.sh
@@ -1,3 +1,10 @@
+/*
+ * Linux DTrace
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 #!/bin/bash
 
 # SYNOPSIS
