a694a917b3d5d2532b7dce9bebcbdd481b9df40c
Eugene Loh <eugene.loh@oracle.com>
2025-10-01 02:24:17 -0400

    test: Fix antiquated reference to dtrace_ustackdepth_max
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/triggers/ustack-tst-bigstack.c
+++ test/triggers/ustack-tst-bigstack.c
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2007, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -10,6 +10,10 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+/*
+ * Create a ridiculously large stack - enough to push us over
+ * the default setting of 'kernel.perf_event_max_stack' (127).
+ */
 int limit = 4096;
 
 int grow1(int);
@@ -23,14 +27,6 @@ shouldGrow(int frame)
 int
 grow(int frame)
 {
-	/*
-	 * Create a ridiculously large stack - enough to push us over
-	 * the default setting of 'dtrace_ustackdepth_max' (2048).
-	 *
-	 * This loop used to repeatedly call getpid(), but on Linux the result
-	 * of that call gets cached, so that repeated calls actually do not
-	 * trigger a system call anymore.  We use ioctl() instead.
-	 */
 	if (shouldGrow(frame))
 		frame = grow1(frame++);
 
