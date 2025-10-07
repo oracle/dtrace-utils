bf23642faa29e212b114063f2e43f713ae54cc28
Eugene Loh <eugene.loh@oracle.com>
2025-10-07 18:55:43 -0400

    test: Modify fbtsafety tests that use vtimestamp unnecessarily
    Currently, we do not implement vtimestamp.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/stress/fbtsafety/tst.strstr.d
+++ test/stress/fbtsafety/tst.strstr.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2025, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -17,7 +17,7 @@ fbt:::
 fbt:::
 /on/
 {
-	trace(strstr((char *)rand(), (char *)(rand() ^ vtimestamp)));
+	trace(strstr((char *)rand(), (char *)(rand() ^ timestamp)));
 }
 
 fbt:::entry
--- test/stress/fbtsafety/tst.substr.d
+++ test/stress/fbtsafety/tst.substr.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2006, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2025, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -17,13 +17,13 @@ fbt:::
 fbt:::
 /on/
 {
-	trace(substr((char *)rand(), rand() ^ vtimestamp));
+	trace(substr((char *)rand(), rand() ^ timestamp));
 }
 
 fbt:::
 /on/
 {
-	trace(substr((char *)rand(), -rand() ^ vtimestamp));
+	trace(substr((char *)rand(), -rand() ^ timestamp));
 }
 
 tick-1sec
