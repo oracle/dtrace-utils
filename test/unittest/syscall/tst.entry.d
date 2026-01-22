ee89990bd8366d3d04e94db8c082936a6ef7d3ee
Eugene Loh <eugene.loh@oracle.com>
2026-01-22 12:37:54 -0800

    test: Increase syscall entry timeout
    The run time for this test seems twice as long for aarch64 as for
    x86_64.  Further, the run time seems to have jumped significantly from
    kernel 5.15 to 6.12 and then again to kernel 6.18.  E.g.,
    
                      x86_64     aarch64
        5.15          7 secs     18 secs
        6.12         12 secs     33 secs
        6.18         22 secs     54 secs
    
    Looking at a run on the 6.18 aarch64 system, the time is basically
    spent in the dtrace_close() call to dt_probe_detach_all(), which does:
    
        for (prp = ...)
            prp->prov->impl->detach(dtp, prp);
    
    Then, dt_tp_probe_detach() calls dt_tp_detach(), which does:
    
        close(tpp->fd);
    
    This close() averages over 0.1 secs.  For hundreds of syscall probes,
    we get nearly a minute of run time, exceeding the default test timeout.
    
    For the time being, increase the timeout on this test.
    
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>

--- test/unittest/syscall/tst.entry.d
+++ test/unittest/syscall/tst.entry.d
@@ -1,6 +1,6 @@
 /*
  * Oracle Linux DTrace.
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2026, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
@@ -8,6 +8,7 @@
 /* @@trigger: syscall-tst-args */
 /* @@trigger-timing: before */
 /* @@runtest-opts: $_pid */
+/* @@timeout: 80 */
 
 /* check that syscall:::entry picks up mmap:entry */
 /* (mmap is called repeatedly by the trigger) */
